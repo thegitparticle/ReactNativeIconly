a0419d944dd5bf9d98aeaa7c37daa3edd31117b4
SAN <maddamsettysanjit@gmail.com>
2021-06-13 00:07:54 +0530

    new way of exporting

--- App.js
+++ App.js
@@ -9,19 +9,6 @@
 import React from 'react';
 import type {Node} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import MyIcon from 'react-native-custom-icon';
-import IcomoonConfig from './assets/Iconly/selection.json';
-
-export const Iconly = props => {
-  return (
-    <MyIcon
-      name={props.name}
-      color={props.color}
-      size={props.size}
-      config={IcomoonConfig}
-    />
-  );
-};
 
 const App: () => Node = () => {
   return (
--- index.js
+++ index.js
@@ -3,8 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App, {Iconly} from './App';
+import App from './App';
 import {name as appName} from './app.json';
+import Iconly from './src/Main';
 
 export {Iconly};
+
 AppRegistry.registerComponent(appName, () => App);
